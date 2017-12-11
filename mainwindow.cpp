6889d1a4ee62d3c6da8403eee9f224c4a5bc9826
Florian <soniakaflo@web.de>
2017-12-11 19:32:50 +0100

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,31 +6,48 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->sendBtn, SIGNAL(clicked()),this, SLOT(sendMail()));
+    connect(ui->exitBtn, SIGNAL(clicked()),this, SLOT(close()));
+    connect(ui->browseBtn, SIGNAL(clicked()), this, SLOT(browse()));
+}
 
+void MainWindow::browse()
+{
+    files.clear();
 
+    QFileDialog dialog(this);
+    dialog.setDirectory(QDir::homePath());
+    dialog.setFileMode(QFileDialog::ExistingFiles);
+
+    if (dialog.exec())
+        files = dialog.selectedFiles();
+
+    QString fileListString;
+    foreach(QString file, files)
+        fileListString.append( "\"" + QFileInfo(file).fileName() + "\" " );
+
+    ui->file->setText( fileListString );
+
+}
+
+void MainWindow::sendMail()
+{
+    Smtp* smtp = new Smtp(ui->uname->text(), ui->paswd->text(), ui->server->text(), ui->port->text().toInt());
+    connect(smtp, SIGNAL(status(QString)), this, SLOT(mailSent(QString)));
+
+    if( !files.isEmpty() )
+        smtp->sendMail(ui->uname->text(), ui->rcpt->text() , ui->subject->text(),ui->msg->toPlainText(), files );
+    else
+        smtp->sendMail(ui->uname->text(), ui->rcpt->text() , ui->subject->text(),ui->msg->toPlainText());
+}
+
+void MainWindow::mailSent(QString status)
+{
+    if(status == "Message sent")
+        QMessageBox::warning( 0, tr( "Qt Simple SMTP client" ), tr( "Message sent!\n\n" ) );
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
-}
-
-void MainWindow::on_actionAusleihen_triggered()
-{
-    ui->stackedWidget->setCurrentIndex(1);
-}
-
-void MainWindow::on_actionKatalog_triggered()
-{
-    ui->stackedWidget->setCurrentIndex(2);
-}
-
-void MainWindow::on_actionNeuer_Katalogeintrag_triggered()
-{
-    ui->stackedWidget->setCurrentIndex(3);
-}
-
-void MainWindow::on_actionHome_triggered()
-{
-    ui->stackedWidget->setCurrentIndex(0);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "smtp.h"
+#include <QtWidgets/QMessageBox>
+#include <QFileDialog>
 
 namespace Ui {
 class MainWindow;
@@ -10,22 +13,20 @@ class MainWindow;
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
+    
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
 private slots:
-    void on_actionAusleihen_triggered();
-
-    void on_actionKatalog_triggered();
-
-    void on_actionNeuer_Katalogeintrag_triggered();
-
-    void on_actionHome_triggered();
-
+    void sendMail();
+    void mailSent(QString);
+    void browse();
+    
 private:
     Ui::MainWindow *ui;
+    QStringList files;
+
 };
 
 #endif // MAINWINDOW_H
