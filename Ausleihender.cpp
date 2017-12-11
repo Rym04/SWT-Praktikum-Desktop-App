ec593e7a45cf9f8a54059195df30f508ace96208
fatimabela <33935668+fatimabela@users.noreply.github.com>
2017-12-11 14:08:41 +0100

    Add files via upload

---
+++ Ausleihender.cpp
@@ -0,0 +1,15 @@
+#include "Ausleihender.h"
+#include <string>
+using namespace std;
+class Ausleihender : Person
+{
+public:
+    string kommentar;
+
+    Ausleihender::Ausleihender(string k) : kommentar(k){}
+
+};
+
+
+
+
---
+++ Bearbeiter.h
@@ -0,0 +1,11 @@
+#ifndef BEARBEITER_H
+#define BEARBEITER_H
+
+
+class Bearbeiter
+{
+public:
+    Bearbeiter();
+};
+
+#endif // BEARBEITER_H
